d8779de96a711c41d623dc8c883d18c9f935e1fd
Raul Dronca <rauldronk@gmail.com>
2019-12-05 16:14:27 +0200

    added react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,82 +1,37 @@
-import React, { useState, useRef } from 'react'
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { Canvas, extend, useThree, useRender } from 'react-three-fiber'
-import { useSpring, a } from 'react-spring/three'
-
+// Components
 import Main from './Main'
 import Title from './Title'
 import Sidebar from './Sidebar'
 import Container from './Container'
 import GlobalStyle from './Global'
 
-extend({ OrbitControls })
-
-const Controls = () => {
-	const orbitRef = useRef()
-	const { camera, gl } = useThree()
-
-	useRender(() => {
-		orbitRef.current.update()
-	})
-
-	return (
-		<orbitControls
-			autoRotate
-			maxPolarAngle={Math.PI / 3}
-			minPolarAngle={Math.PI / 3}
-			args={[camera, gl.domElement]}
-			ref={orbitRef}
-		/>
-	)
-}
-
-const Box = () => {
-	const [hovered, setHover] = useState(false)
-	const [active, setActive] = useState(false)
-	const props = useSpring({
-		scale: active ? [1.5, 1.5, 1.5] : [1, 1, 1],
-		color: hovered ? 'skyblue' : 'tomato',
-	})
-
-	return (
-		<a.mesh
-			onPointerOver={() => setHover(true)}
-			onPointerOut={() => setHover(false)}
-			onClick={() => setActive(!active)}
-			scale={props.scale}
-			castShadow
-		>
-			<ambientLight />
-			<spotLight position={[0, 5, 10]} penumbra={1} castShadow />
-			<boxBufferGeometry attach='geometry' args={[1, 1, 1]} />
-			<a.meshPhysicalMaterial attach='material' color={props.color} />
-		</a.mesh>
-	)
-}
+// Experiments
+import FrontPage from '../experiments/FrontPage'
+import SimpleCube from '../experiments/SimpleCube'
 
 const App = () => {
 	return (
 		<>
 			<GlobalStyle />
 			<Container>
-				<Sidebar>
-					<Title>3D Experiments</Title>
-				</Sidebar>
-				<Main>
-					<Canvas
-						camera={{ position: [0, 0, 5] }}
-						onCreated={({ gl }) => {
-							gl.shadowMap.enabled = true
-							gl.shadowMap.type = THREE.PCFSoftShadowMap
-						}}
-					>
-						<fog attach='fog' args={['white', 5, 15]} />
-						<Controls />
-						<Box />
-					</Canvas>
-				</Main>
+				<Router>
+					<Sidebar>
+						<Title>
+							<Link to='/'>3D Experiments</Link>
+						</Title>
+
+						<Link to='/simple-cube'>Simple Cube</Link>
+					</Sidebar>
+					<Main>
+						<Switch>
+							<Route path='/' exact component={FrontPage} />
+							<Route path='/simple-cube' component={SimpleCube} />
+						</Switch>
+					</Main>
+				</Router>
 			</Container>
 		</>
 	)
