fb3fef61538a25559e0c5410c3d80db1e54ef9ec
Raul Dronca <rauldronk@gmail.com>
2019-12-06 15:44:28 +0200

    content for intro page

--- src/experiments/Intro.js
+++ src/experiments/Intro.js
@@ -18,15 +18,10 @@ const Text = styled.p`
 
 const Intro = () => (
 	<About>
-		<Title>Hello World</Title>
+		<Title>3D Experiments</Title>
 		<Text>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-			veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-			commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-			velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-			cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-			est laborum.
+			This is a place where I post sketches and experiments while learning
+			THREE.js.
 		</Text>
 	</About>
 )
